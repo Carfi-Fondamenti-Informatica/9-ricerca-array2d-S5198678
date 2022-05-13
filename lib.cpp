a7424c3b7779e880466ea5fa8fea8ebc72978e23
S5198678 <100881826+S5198678@users.noreply.github.com>
2022-05-13 22:03:02 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,35 @@
 
+#include "lib.h"
+
+void init(char mat[10][20]){
+    for (int i=0; i<10; i++){
+        for (int j=0; j<20; j++){
+            mat[i][j]=0;
+        }
+    }
+}
+
+void init(char arr[20]){
+    for (int i=0; i<20; i++){
+        arr[i]=0;
+    }
+}
+
+int ricerca(char nome[10][20], char n[20]){
+    int x;
+    for (int i=0; i<10; i++){
+        x=0;
+        for (int j=0; j<20; j++){
+            if(nome[i][j]==n[j]){
+                x++;
+            }else{
+                break;
+            }
+        }
+        if(x==20){
+            return i;
+        }
+    }
+    return -1;
+
+}
