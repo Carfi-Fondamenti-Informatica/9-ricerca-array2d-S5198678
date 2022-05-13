954e0a18c6753c2a06065e7b2c00f51574acc8a7
S5198678 <100881826+S5198678@users.noreply.github.com>
2022-05-13 22:04:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,17 +2,24 @@
 #include "lib.h"
 using namespace std;
 
-  int main(){
-      int pos;
-      char a[10][20], f[1][20];
-      for (int i=0; i<10; i++){
-        cin >> a[i];
-        }
-    cin >> f[0];
-    if (ricerca(a,f,pos)){
-     cout << pos << endl;
- }else{
-      cout << "non presente" << endl;
-    }  
-  return 0;
+int main(){
+    char nome[10][20];
+    char n[20];
+    init(nome);
+    init(n);
+
+    for (int i=0; i<10; i++){
+        cin >> nome[i];
+    }
+   
+    cin >> n;
+
+    int a= ricerca(nome,n);
+    if(a!=-1){
+        cout<<a;
+    } else{
+        cout<<"non presente"<<endl;
+    }
+
+    return 0;
 }
