635d238e2582118f88f625b1e6f414365ba5fb14
S5198678 <100881826+S5198678@users.noreply.github.com>
2022-05-13 22:03:42 +0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,6 +1,5 @@
-#ifndef UNTITLED17_LIB_H
-#define UNTITLED17_LIB_H
 
-bool ricerca(chae lista [10][20], char nome[1][20], int &pos);
 
-#endif //UNTITLED17_LIB_H
+void init(char [10][20]);
+void init(char [20]);
+int ricerca(char [10][20], char [20]);
